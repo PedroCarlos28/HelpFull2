6777e8a0a91773908f115abd07af2e2165c6c31a
Pedro Carlos <pedro.carlos2803@gmail.com>
2026-09-25 12:53:15 -0300

    fix: atividades cabecalho usa overlay de gradiente em vez de filter, textos e abas ficam legíveis

--- Perfil.php
+++ Perfil.php
@@ -1529,7 +1529,7 @@ $abrevEmocoes = ['Irritado' => 'Irri.', 'Ansioso' => 'Ansi.', 'Feliz' => 'Feli.'
             .toast-notificacao { width: 92%; max-width: 380px; }
         }
     </style>
-    <link rel="stylesheet" href="assets/acessibilidade.css?v=20260925-v12">
+    <link rel="stylesheet" href="assets/acessibilidade.css?v=20260925-v13">
 </head>
 
 <body>
