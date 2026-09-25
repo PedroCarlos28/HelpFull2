adce6b75947943d0b378cf241b46e0971c35beef
Pedro Carlos <pedro.carlos2803@gmail.com>
2026-09-25 12:34:21 -0300

    fix: unifica paleta modo escuro - sem caixas visiveis, tons mais proximos, textarea e comunidade consistentes

--- ChatBOT.php
+++ ChatBOT.php
@@ -1112,7 +1112,7 @@ $datasHistorico = $stmtHist->fetchAll(PDO::FETCH_COLUMN);
             }
         }
     </style>
-    <link rel="stylesheet" href="assets/acessibilidade.css?v=20260925-v9">
+    <link rel="stylesheet" href="assets/acessibilidade.css?v=20260925-v10">
 </head>
 
 <body>
